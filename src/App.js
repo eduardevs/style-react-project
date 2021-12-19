45b6c5f5ec6a633471f1f38b28da0764d8f973b3
AnyName <any@email.com>
2021-12-20 00:02:23 +0100

    Set project base to practice

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     return (
         <div>
             <Home />
+            <p>hi</p>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-
+// this is the index
 
 ReactDOM.render(<App />, document.getElementById('root'))
