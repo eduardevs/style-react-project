7056381ae734dd3d1d92f5d7b24c44c60d7830bf
Eduardo Pina <80246060+jeseduardopi@users.noreply.github.com>
2021-12-20 00:42:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-// this is the index
+// this is the index.  NICE TEST
 
 ReactDOM.render(<App />, document.getElementById('root'))
